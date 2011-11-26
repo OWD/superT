11100ccf9a12a1d3d55011d2490a6652b7da359c
fergost bro <fergost@gmail.com>
2011-11-26 17:40:07 +0000

    dos menus

--- js/fuentesSelector.js
+++ js/fuentesSelector.js
@@ -1,15 +1,32 @@
 
 var replaceFonts=(function()
 {
-        document.selectores={
+        document.selectores["index"]={
             "ul.oe_menu div a" : "Aller",
-            "#miIde" : "AcornInitials"  
+            "#miIde" : "AcornInitials"
             };
+        document.selectores["galeria"]={
+            "ul.oe_menu div a" : "Aller",
+            "#miIde" : "AcornInitials"
+            };
+        document.selectores["menu"]={
+            "span li h1 p" : "LiberationSans",
+            ".cc_content_1" : "AcornInitials"
+        };
+            
+            
+            
         
         
+        $.each(document.selectores["index"],(function(selector,fuente){
+                
+                Cufon.replace(selector,fuente);
+        }));
         
-        $.each(document.selectores,(function(selector,fuente){
+        $.each(document.selectores["menu"],(function(selector,fuente){
+                
                 Cufon.replace(selector,fuente);
         }));
         
 });
+
--- js/script.js
+++ js/script.js
@@ -27,5 +27,148 @@ var instrumentalizeMenuItems=(function()
             $this.removeClass('hovered');
             $oe_overlay.stop(true,true).fadeTo(200, 0);
             $oe_menu_items.children('div').hide();
-    })
+    });
+});
+    
+    
+ // de aqui para abajo son funciones del menu one   
+
+var  menuoneInitial1=(function()
+{
+    ///**
+    //bind the mouseenter, mouseleave to each item:
+    //- shows / hides image and submenu
+    //bind the click event to the list elements (submenu):
+    //- hides all items except the clicked one, 
+    //and shows the content for that item
+    //*/
+    $items.unbind('mouseenter')
+      .bind('mouseenter',m_enter)
+      .unbind('mouseleave')
+      .bind('mouseleave',m_leave)
+      .find('.cc_submenu > ul > li')
+      .bind('click',function(){
+    var $li_e = $(this);
+     //if the menu is already folded,
+     //just replace the content
+    if(folded){
+	hideContent();
+	showContent($li_e.attr('class'));
+    }	
+    else //fold and show the content
+     {
+	fold($li_e);
+     }
+      });
+});
+	
+//back to menu button - unfolds the menu
+var menuoneInitial2=(function(){
+    $('#cc_back').bind('click',unfold);
+});
+    ///**
+    //hides all the menu items except the clicked one
+    //after that, the content is shown
+    //*/
+    
+    
+    ///**
+    //mouseenter function for the items
+    //the timeout is used to prevent this event 
+    //to trigger if the user moves the mouse with 
+    //a considerable speed through the menu items
+    //*/
+var m_enter=(function()
+    {
+        var $this 	= $(this);
+        clearTimeout(menu_time);
+        menu_time = setTimeout(function(){
+        	//img
+                $this.find('img').stop().animate({'top':'0px'},400);
+                //cc_submenu ul
+                $this.find('.cc_submenu > ul').stop().animate({'height':'200px'},400);
+            },200);
+    });
+				
+//mouseleave function for the items
+var m_leave=(function()
+    {
+    	var $this = $(this);
+	clearTimeout(menu_time);
+	//img
+	$this.find('img').stop().animate({'top':'-600px'},400);
+	//cc_submenu ul
+	$this.find('.cc_submenu > ul').stop().animate({'height':'0px'},400);
+    });
+
+	
+  
+var fold=(function($li_e)
+{
+	var $item		= $li_e.closest('.cc_item');		
+	var d = 100;
+	var step = 0;
+	$items.unbind('mouseenter mouseleave');
+	$items.not($item).each(function(){
+            var $item = $(this);
+            $item.stop().animate({
+		'marginLeft':'-140px'
+            },d += 200,function(){
+                ++step;
+                if(step == cnt_items-1){
+                    folded = true;
+                    showContent($li_e.attr('class'));
+                }	
+            });
+        });
+});
+				
+    ///**
+    //shows all the menu items 
+    //also hides any item's image / submenu 
+    //that might be displayed
+    //*/
+var unfold=(function()
+{
+	$('#cc_content').stop().animate({'left':'-700px'},600,function(){
+	 var d = 100;
+	 var step = 0;
+         $items.each(function(){
+		var $item = $(this);			
+		$item.find('img')
+                 .stop()
+		 .animate({'top':'-600px'},200)
+		 .andSelf()
+		 .find('.cc_submenu > ul')
+		 .stop()
+		 .animate({'height':'0px'},200);		 
+                 $item.stop().animate({
+			'marginLeft':'0px'
+		},d += 200,function(){
+			++step;
+			if(step == cnt_items-1){
+			folded = false;
+			$items.unbind('mouseenter')
+                         .bind('mouseenter',m_enter)
+			 .unbind('mouseleave')
+			 .bind('mouseleave',m_leave);
+			hideContent();
+		}		  
+	    });
+	});
+    });
+});
+
+    //function to show the content
+var showContent=(function(idx)
+{
+    $('#cc_content').stop().animate({'left':'140px'},200,function(){
+		$(this).find('.'+idx).fadeIn();
+    });
+});
+				
+    //function to hide the content
+var hideContent=(function()
+{
+	$('#cc_content').find('div').hide();
 });
